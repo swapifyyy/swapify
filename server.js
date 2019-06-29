01eebf0134011121413d85b94c1706d33560a882
Cris <cris.kgl@gmail.com>
2019-06-29 13:14:26 +0000

    RESTART POINT TO HEXAGONS

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ var client_secret = '32ab56c55e5640b0b3a4855a0959f251'; // Your secret
 * for deploying localy in localhost:8888 just comment the seond 
 * delaration of redirect uri
 */
-//var redirect_uri =  'http://localhost:8888/home'; // Your redirect uri
-var redirect_uri =  'https://swapify123.herokuapp.com/home';
+var redirect_uri =  'http://localhost:8888/home'; // Your redirect uri
+//var redirect_uri =  'https://swapify123.herokuapp.com/home';
 
 console.log(redirect_uri);
 /**
